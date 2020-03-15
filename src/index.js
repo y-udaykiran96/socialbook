86f3fe789a789d977b9b6d7d3f6694e863dac7c6
y-udaykiran96 <y.udaykiran96@gmail.com>
2020-03-15 21:52:24 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import './assets/css/style.css'
+import './assets/css/animate.css'
+// import './assets/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/line-awesome.css'
+import './assets/css/line-awesome-font-awesome.min.css'
+import './assets/vendor/fontawesome-free/css/all.min.css'
+import './assets/css/font-awesome.min.css'
+import './assets/css/jquery.mCustomScrollbar.min.css'
+// import './assets/lib/slick/slick.css'
+import "slick-carousel/slick/slick.css";
+// import './assets/lib/slick/slick-theme.css'
+import "slick-carousel/slick/slick-theme.css";
+import './assets/css/responsive.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

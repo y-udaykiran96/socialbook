98fb9b8f69010dddf63559c0e38d90819c5c631b
y-udaykiran96 <y.udaykiran96@gmail.com>
2020-04-10 23:13:38 +0530

    adding script tag in html-page with component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Main from './components/main'
 import Header from './components/header'
 
 // import $ from 'jquery';
 // import Popper from 'popper.js';
 
-function App() {
-  return (
-    <div className="App wrapper">
-      <Header />
-      <Main />
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "http://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js";
+
+    document.head.appendChild(script);  
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+      </React.Fragment>
+    )
+  }
+}
